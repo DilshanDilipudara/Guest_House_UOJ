b6940383dbd65d6f0e91cc219516e8127d9f395b
sanuja123 <sanuja12lakshitha@gmail.com>
2019-03-10 22:05:26 +0530

    added  room confirm,add user and payment in to admin panel. i corected navigation path error. it is ok now

--- resources/views/paymentinfo1.blade.php
+++ resources/views/paymentinfo1.blade.php
@@ -1,88 +1,14 @@
-<!doctype html>
+
+<!DOCTYPE html>
+
 <html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon.png">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-	<title>Admin Panel</title>
-
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
-
-
-    <!-- Bootstrap core CSS     -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" />
-
-    <!-- Animation library for notifications   -->
-    <link href="css/animate.min.css" rel="stylesheet"/>
-
-    <!--  Paper Dashboard core CSS    -->
-    <link href="css/paper-dashboard.css" rel="stylesheet"/>
-
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="css/demo.css" rel="stylesheet" />
-
-    <!--  Fonts and icons     -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Muli:400,300' rel='stylesheet' type='text/css'>
-    <link href="css/themify-icons.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet">
-
-</head>
-<body>
-
-<div class="wrapper">
-	<div class="sidebar" data-background-color="black" data-active-color="danger">
-
-    <!--
-		Tip 1: you can change the color of the sidebar's background using: data-background-color="white | black"
-		Tip 2: you can change the color of the active button using the data-active-color="primary | info | success | warning | danger"
-	-->
-	<title>Admin Panel</title>
-
-    	<div class="sidebar-wrapper">
-            <div class="logo">
-                <a href="confirmlist" class="simple-text">
-                    Admin Panel
-                </a>
-            </div>
-
-            
-            <?php include '..\resources\views\includes\navbar.php'?>
-            
-        
-        
-    	</div>
-    </div>
-
-    <div class="main-panel">
-		<nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar bar1"></span>
-                        <span class="icon-bar bar2"></span>
-                        <span class="icon-bar bar3"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Payment Information.</a>
-                </div>
-                
-            </div>
-        </nav>
-
-
-        <div class="content">
-       <!-- Add table content here -->
-        
-
 
 <head>
 
 
-	
+	<title>Table V02</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
@@ -103,7 +29,19 @@
 	
 <!--===============================================================================================-->
 <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
-  
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/customcss.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
@@ -118,7 +56,7 @@
 
 
     <div class="card  card-title text-center">
-    
+    <h1> Bill Payment</h1>
 	</br>
 	<div class="container mt-3">
 	<input class="form-control" id="myInput" type="text" placeholder="Search..">
@@ -184,42 +122,3 @@ $(document).ready(function(){
 
 </body>
 </html>
-
-
-        </div>
-
-
-
-    </div>
-</div>
-
-
-</body>
-
-    <!--   Core JS Files   -->
-    <script src="js/jquery.min.js" type="text/javascript"></script>
-	<script src="js/bootstrap.min.js" type="text/javascript"></script>
-
-	<!--  Checkbox, Radio & Switch Plugins -->
-	<script src="js/bootstrap-checkbox-radio.js"></script>
-
-	<!--  Charts Plugin -->
-	<script src="js/chartist.min.js"></script>
-
-    <!--  Notifications Plugin    -->
-    <script src="js/bootstrap-notify.js"></script>
-
-    <!--  Google Maps Plugin    -->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
-
-    <!-- Paper Dashboard Core javascript and methods for Demo purpose -->
-	<script src="js/paper-dashboard.js"></script>
-
-	<!-- Paper Dashboard DEMO methods, don't include it in your project! -->
-	<script src="js/demo.js"></script>
-
-</html>
-
-<!-- ******************************** jquery *********************************** -->
-
-
